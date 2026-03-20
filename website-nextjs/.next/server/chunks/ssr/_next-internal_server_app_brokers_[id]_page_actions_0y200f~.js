2650c7362711a948d65d14d30e9498503f565993
ankuromen <ankur.patel07@outlook.com>
2026-03-20 18:55:43 +0530

    chore: remove Next.js build artifacts from git

--- website-nextjs/.next/server/chunks/ssr/_next-internal_server_app_brokers_[id]_page_actions_0y200f~.js
+++
@@ -1,3 +0,0 @@
-module.exports=[67698,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_brokers_%5Bid%5D_page_actions_0y200f~.js.map
--- website-nextjs/.next/server/chunks/ssr/_next-internal_server_app_location_[city]_[locality]_page_actions_0p.qe-s.js
+++
@@ -1,3 +0,0 @@
-module.exports=[51201,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_location_%5Bcity%5D_%5Blocality%5D_page_actions_0p.qe-s.js.map
